b5d140fe76e209473dfe08fef2ffb33cb977a55f
Mishal Islam <mishal.islam01@gmail.com>
2015-02-06 16:05:45 +0000

    refactored

--- lib/split.rb
+++ lib/split.rb
@@ -1,22 +1,44 @@
 class SplitNumbers
 
   def convertToArray(number)
-    array = []
-    value = 1000
-    divisor = 2
-
-    until number === 0
-      unit = number / value
-      array << unit * value
-      number -= unit * value
-      value = value / divisor
-      divisor === 2 ? divisor = 5 : divisor = 2
-    end
-    array
+    setup(number)
+    loopThroughNumber(number)
   end
 
-  def setup
-    
+  def setup(number)
+    variables(number)
+    @array = []
+  end
+
+  def variables(number)
+    @value = 1000
+    @divisor = 2
+    @number = number
+  end
+
+  def loopThroughNumber(number)
+    something(@number) until @number === 0
+    @array
+  end
+
+  def something(number)
+    seperateNumbers
+    @divisor === 2 ? @divisor = 5 : @divisor = 2
+  end
+
+  def unitMultipliedByValue(unit)
+    unit * @value
+  end
+
+  def seperateNumbers
+    unit = @number / @value
+    pushNumberIntoArray(unit)
+    @value = @value / @divisor
+  end
+
+  def pushNumberIntoArray(unit)
+    @array << unitMultipliedByValue(unit)
+    @number -= unitMultipliedByValue(unit)
   end
 
 end
