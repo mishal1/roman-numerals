3ad07ded73c840b7f0ae3adcab93f5206134ba21
Mishal Islam <mishal.islam01@gmail.com>
2015-02-06 12:41:01 +0000

    deals with subracting numbers

--- lib/roman.rb
+++ lib/roman.rb
@@ -13,27 +13,39 @@ class RomanNumerals
   end
 
   def under10s(number)
-    number < 5 ? convertToI(number) : 'V'
+    number < 5 ? convertToI(number) : convertToV(number)
   end
 
   def under100s(number)
-    number < 50 ? convertToX(number) : 'L'
+    number < 50 ? convertToX(number) : convertToL(number)
   end
 
   def under1000s(number)
-    number < 500 ? convertToC(number) : 'D'
+    number < 500 ? convertToC(number) : convertToD(number)
   end
 
   def convertToI(number)
     number < 4 ? 'I' * number : 'IV'
   end
 
+  def convertToV(number)
+    number < 9 ? 'V' : 'IX'
+  end
+
   def convertToX(number)
-   return 'X' * (number / 10) if number
+    number < 40 ? 'X' * (number / 10) : 'XL'
+  end
+
+  def convertToL(number)
+    number < 90 ? 'L' : 'XC'
   end
 
   def convertToC(number)
-    'C' * (number / 100)
+    number < 400 ? 'C' * (number / 100) : 'CD'
+  end
+
+  def convertToD(number)
+    number < 900 ? 'D' : 'CM'
   end
 
   def convertToM(number)
--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -52,8 +52,24 @@ describe RomanNumerals do
     expect(roman.convertArabicToRoman(4)).to eq 'IV'
   end
 
-  it 'says "IX" if the number is 4' do
+  it 'says "IX" if the number is 9' do
     expect(roman.convertArabicToRoman(9)).to eq 'IX'
+  end
+
+  it 'says "XL" if the number is 40' do
+    expect(roman.convertArabicToRoman(40)).to eq 'XL'
+  end
+
+  it 'says "XC" if the number is 90' do
+    expect(roman.convertArabicToRoman(90)).to eq 'XC'
+  end
+
+  it 'says "CD" if the number is 400' do
+    expect(roman.convertArabicToRoman(400)).to eq 'CD'
+  end
+
+  it 'says "CM" if the number is 900' do
+    expect(roman.convertArabicToRoman(900)).to eq 'CM'
   end
 
 end
