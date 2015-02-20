052fabf1671348c81bcb8030de68e431cb5aa94c
Mishal Islam <mishal.islam01@gmail.com>
2015-02-20 09:55:47 +0000

    better less complicated version using symbols

--- lib/roman.rb
+++ lib/roman.rb
@@ -1,61 +1,17 @@
 class RomanNumerals
 
-  def convertArabicToRoman(number, splitNumbers)
-    @romanNumeral = []
-    loopThroughSplitNumbers(number, splitNumbers)
-    @romanNumeral.join
-  end
+def initialize(hash)
+  @romanHash = hash
+end
 
-  def loopThroughSplitNumbers(number, splitNumbers)
-      splitNumbers.convertToArray(number).each {|number| number < 100 ? oneToOneHundred(number) : overOneHundred(number)}
+def convertArabicToRoman(arabicNumber)
+  roman = ''
+  @romanHash.each do |key, value| 
+    roman << value * (arabicNumber / key) 
+    divisor = arabicNumber / key
+    arabicNumber -= divisor * key
   end
-
-  def oneToOneHundred(number)
-    number < 10 ? under10s(number) : under100s(number)
-  end
-
-  def overOneHundred(number)
-    number < 1000 ? under1000s(number) : convertToM(number)
-  end
-
-  def under10s(number)
-    number < 5 ? convertToI(number) : convertToV(number)
-  end
-
-  def under100s(number)
-    number < 50 ? convertToX(number) : convertToL(number)
-  end
-
-  def under1000s(number)
-    number < 500 ? convertToC(number) : convertToD(number)
-  end
-
-  def convertToI(number)
-    number < 4 ? @romanNumeral << 'I' * number : @romanNumeral << 'IV'
-  end
-
-  def convertToV(number)
-    number < 9 ? 'V' : 'IX'
-  end
-
-  def convertToX(number)
-    number < 40 ? @romanNumeral << 'X' * (number / 10) : @romanNumeral << 'XL'
-  end
-
-  def convertToL(number)
-    number < 90 ? @romanNumeral << 'L' : @romanNumeral << 'XC'
-  end
-
-  def convertToC(number)
-    number < 400 ? @romanNumeral << 'C' * (number / 100) : @romanNumeral << 'CD'
-  end
-
-  def convertToD(number)
-    number < 900 ? @romanNumeral << 'D' : @romanNumeral << 'CM'
-  end
-
-  def convertToM(number)
-    @romanNumeral << 'M' * (number / 1000)
-  end
+ roman
+end
 
 end
